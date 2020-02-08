f2f91d621fe8b4952606a172f8a1388dae185d53
NintySwinty <31569763+NinjaGames4256@users.noreply.github.com>
2020-02-08 12:19:15 +0100

    BaseStrony
    HTML

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+
+    <head>
+
+        <meta charset="UTF-8"; name="Opis" content="Strona zespołu szkół nr.42 imienia Jana Karskiego"; name="keywords" content="Szkoła, Karski, 42, Zespół, szkół, Technikum, Liceum">
+
+        <title> Zespół Szkół nr.42 imienia Jana Karskiego </title>
+
+    </head>
+
+    <body>
+
+        <div align = "center"; id="Background">
+        
+            <h1 aling = "center"> Strona główna | Rodo | Konkursy | Dla nauczycieli | Dla uczniów </h1>
+
+        </div>
+
+        <div align = "center"; id="Photo" >
+        
+            <img src="./Image/images.jpeg">
+
+        </div>
+
+        <div align = "center"; id="Background">
+        
+            <h1> i informację </h1>
+
+        </div>
+
+        <div align = "left"; id="Foreground" >
+        
+            <a> <h3> Podstrony </h3> </a> <br>
+            <a> <h3> Technik Informatyk </h3> </a> <br>
+            <a> <h3> Technik Turystyki </h3> </a> <br>
+            <a> <h3> Oddział dla młodocianych pracowników </h3> </a>
+
+        </div>
+    
+    </body>
+
+</html>
